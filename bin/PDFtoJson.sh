cb73a83442f2a212551796d8f2b8d5e9c9e8c027
RouxRC <b.ooghe@gmail.com>
2015-12-07 04:06:23 +0100

    rename script for one

--- bin/PDFtoJson.sh
+++ bin/PDFtoJson.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd $(dirname $0)/..
+
 PDF="$1"
 SOURCE=$(echo "$PDF" | sed 's/\.pdf$//')
 OUT=$(echo "$SOURCE" | sed -r 's|^.*/([^/]*)$|\1|')
 
 mkdir -p data/DOIs
 pdftohtml -xml "$PDF" > /dev/null
-bin/scrap.py "$SOURCE".xml > data/DOIs/"$OUT".json
+bin/PDFtoJson.py "$SOURCE".xml > data/DOIs/"$OUT".json
 rm -f "$SOURCE".xml
 rm -f $SOURCE-*.png
 
